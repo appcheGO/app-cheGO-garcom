19daf630b41bfdb8b5f58aea1ef42a31b1163bd3
Ezequiel Brilhante <ezequielrb7@gmail.com>
2023-11-11 11:54:03 -0300

    configurações iniciais

--- src/main.jsx
+++ src/main.jsx
@@ -3,58 +3,58 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-import {
-  // eslint-disable-next-line no-unused-vars
-  Outlet,
-  RouterProvider,
-  createBrowserRouter,
-} from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import Order from './pages/Order.jsx';
-import CommandWaiter from './pages/CommandWaiter.jsx';
+// import {
+//   // eslint-disable-next-line no-unused-vars
+//   Outlet,
+//   RouterProvider,
+//   createBrowserRouter,
+// } from 'react-router-dom';
+// import Home from './pages/Home.jsx';
+// import Order from './pages/Order.jsx';
+// import CommandWaiter from './pages/CommandWaiter.jsx';
 // import LoginPage from "../src/admin/src/pages/login.jsx";
 // import AppPage from "./admin/src/pages/app.jsx";
 // Importe a tela de dashboard
 
-const routes = [
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <CommandWaiter />,
-      },
-      {
-        path: '/menu',
-        element: <Home />,
-      },
-      {
-        path: '/pedido',
-        element: <Order />,
-      },
-    ],
-  },
-  // {
-  //   path: "/admin",
-  //   element: <Outlet />,
-  //   children: [
-  //     {
-  //       path: "login",
-  //       element: <LoginPage />,
-  //     },
-  //     {
-  //       path: "dashboard",
-  //       element: <AppPage />,
-  //     },
-  //   ],
-  // },
-];
+// const routes = [
+//   {
+//     path: '/',
+//     element: <App />,
+//     children: [
+//       {
+//         path: '/',
+//         element: <CommandWaiter />,
+//       },
+//       {
+//         path: '/menu',
+//         element: <Home />,
+//       },
+//       {
+//         path: '/pedido',
+//         element: <Order />,
+//       },
+//     ],
+//   },
+//   {
+//     path: "/admin",
+//     element: <Outlet />,
+//     children: [
+//       {
+//         path: "login",
+//         element: <LoginPage />,
+//       },
+//       {
+//         path: "dashboard",
+//         element: <AppPage />,
+//       },
+//     ],
+//   },
+// ];
 
-const router = createBrowserRouter(routes);
+// const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import { Box, Container, Typography } from '@mui/material';
+
+export const Login = () => {
+  return (
+    <Container sx={{ backgroundColor: '#fff' }}>
+      <Box>
+        <Typography sx={{ color: '#f46c26' }}>
+          CheGO
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
